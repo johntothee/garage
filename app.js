3ac78118c0badb76af33ed9259a3309e11a84535
John Erhardt <johne.erhardt@gmail.com>
2018-02-21 12:19:17 -0700

    limit token age to two minutes

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.get('/api/garage', function (req, res) {
     var response = processJwt(res, jwtParam);
 
     // valid commands to start are: verify, open-close
-    // @todo: add correct response for verify command
-    // @todo: open command triggers lock and opener pins
+    // @todo: add correct response for verify
+    // @todo: open-close triggers lock and opener pins
     res.send('Welcome to the garage. reponse: ' + response);
   }
 });
@@ -47,6 +47,7 @@ function processJwt(res, token) {
   }
 
   try {
+    // get user's public key
     var cert = fs.readFileSync('./keys/' + user + '-public.pem');
   }
   catch(error) {
@@ -54,8 +55,14 @@ function processJwt(res, token) {
     return false;
   }
 
-  // verify token
-  var response = jwt.verify(token, cert);
+  // verify token with max age of 2 minutes
+  try {
+    var response = jwt.verify(token, cert, {maxAge: '2m'});
+  }
+  catch (error) {
+    console.log(error);
+    return false;
+  }
   console.log('command: ' + response.command);
   return response.command;
 }
