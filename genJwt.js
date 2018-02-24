cef6bcc9a32df16803b2eeeaf4dcdfd3504c7859
John Erhardt <johne.erhardt@gmail.com>
2018-02-24 16:50:53 -0700

    update documentation, add both commands to genJwt.js

--- genJwt.js
+++ genJwt.js
@@ -7,6 +7,8 @@ var cert = fs.readFileSync('./keys/1-private.key');  // get private key
 // sign asynchronously
 // for testing we're only using uid=1
 // verify command verifies a token was received and properly verified.
+// open-close does what you think.
+// jwt.sign({ uid: '1', command: 'verify' }, cert, { algorithm: 'RS256' }, 
 jwt.sign({ uid: '1', command: 'open-close' }, cert, { algorithm: 'RS256' }, function(err, token) {
   console.log(token);
   var decoded = jwt.decode(token);
