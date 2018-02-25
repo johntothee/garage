4823f5d91555319c514f441d793370f8d3fb13f1
John Erhardt <johne.erhardt@gmail.com>
2018-02-24 22:46:36 -0700

    clean up genJwt.js output

--- genJwt.js
+++ genJwt.js
@@ -12,19 +12,16 @@ if (!(commandToUse = process.argv[3])) {
 var cert = fs.readFileSync('./keys/' + user + '-private.key');  // get private key
 
 // sign asynchronously
-// for testing we're only using uid=1
 // verify command verifies a token was received and properly verified.
 // open-close does what you think.
-// jwt.sign({ uid: '1', command: 'verify' }, cert, { algorithm: 'RS256' }, 
 jwt.sign({ uid: user, command:  commandToUse}, cert, { algorithm: 'RS256' }, function(err, token) {
-  console.log(token);
   var decoded = jwt.decode(token);
 
   // get the decoded payload and header
   var decoded = jwt.decode(token, {complete: true});
+  console.log('header:');
   console.log(decoded.header);
+  console.log('payload:');
   console.log(decoded.payload);
-  console.log('uid: ' + decoded.payload.uid);
-  console.log('command: ' + decoded.payload.command);
-  console.log(process.argv[2]);
+  console.log('token: \n' + token);
 });
