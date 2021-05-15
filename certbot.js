db09cf04d9080476dfd95cd77f5e721bffeb0fe2
johntothee <johne.erhardt@gmail.com>
2021-05-15 14:42:41 -0600

    proper way to work with certbot with renewal authenticator set to webroot

--- certbot.js
+++ certbot.js
@@ -1,22 +1,11 @@
-// Run as root and update strings to generate new cert files from letsencrypt.
+#!/usr/bin/env node
 
-const http = require("http");
 const express = require('express');
 var app = express();
 
+app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 
-// Main listener
-app.get('/.well-known/acme-challenge/AO6ycPphSl54CxIO7uTCHDkzohUhpoJ_x_wY2ews230', function (req, res) {
-  res.send('AO6ycPphSl54CxIO7uTCHDkzohUhpoJ_x_wY2ews230.6g79z-U8d9sChjzwmbuxd-1VIA0I9mOdogaEx4edNzM');
-
+app.listen(80, () => {
+  console.log('HTTP server running on port 80');
 });
 
-// serve static files
-// Use with 'authenticator = webroot' and 'webroot_path = [STATIC PATH]' in /etc/letsencrypt/renewal/*.conf
-app.use(express.static('static'));
-
-
-app = http.createServer(app);
-app.listen(80, function () {
-  console.log('Garage app listening on port 80!');
-});
